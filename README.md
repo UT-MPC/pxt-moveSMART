9deb0b7e873333e878d13c2289c5cb59debfc42a
graceleee <leegracew@gmail.com>
2021-03-04 17:06:36 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [moveSMART](https://github.com/UT-MPC/pxt-moveSMART)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open []()
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/** and import
+
+## Edit this project ![Build status badge](https://github.com//workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open []()
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/** and click import
+
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
---
+++ main.ts
@@ -0,0 +1,41 @@
+namespace moveSMART {
+    //variables
+    let step = 0;
+    let counting: Boolean = false;
+
+    /**
+     * TODO: start counting steps
+     */
+    //%block weight=3000
+    export function startCounting(): void {
+        counting = true
+    }
+    /**
+     * TODO: stop counting steps
+     */
+    //%block weight=2000
+    export function stopCounting(): void {
+        counting = false
+    }
+
+    /**
+     * TODO: show number of steps
+     */
+    //%block weight=1000
+    export function showStepCount(): void {
+        basic.showNumber(step_count())
+        basic.pause(100)
+    }
+
+    //helper functions
+    input.onGesture(Gesture.Shake, function () {
+        if (counting) {
+            step += 1
+        }
+    })
+
+    function step_count(): number {
+        return step
+    }
+
+}
